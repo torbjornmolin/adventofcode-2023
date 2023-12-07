678fad95c7c0a2fef20462245f8a42d90fe31100
Torbjörn Molin <torbjorn.molin@hiq.se>
2023-12-07 10:15:26 +0100

    Day7 part2

--- src/day7.rs
+++ src/day7.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use core::num;
 use std::{fs, collections::HashMap};
 
 
@@ -39,7 +40,7 @@ impl Day7 {
             let bid: u32 = captures.get(2).map(|m| m.as_str()).expect("No cards matched").parse().expect("Unable to parse bid");
 
             let mapped_cards: String = map_cards(&cards);
-            let hand_strength: char = get_hand_strength(&mapped_cards);
+            let hand_strength: char = get_hand_strength(&cards);
             let hand_sort_string = format!("{}_{}", hand_strength, mapped_cards);
             hands.push(Hand {
                 bid,
@@ -60,35 +61,204 @@ impl Day7 {
     }
 
     pub fn sum_part_2(self, content: &str) -> u64 {
+        let re = Regex::new(r"(.{5}) (\d*)").expect("Could not create regex.");
 
-        0
+        let mut hands = Vec::<Hand>::new();
+        for line in content.lines() {
+            let captures = re.captures(line).expect("Unable to match regex");
+
+            let cards = captures.get(1).map(|m| m.as_str()).expect("No cards matched").to_string();
+            let bid: u32 = captures.get(2).map(|m| m.as_str()).expect("No cards matched").parse().expect("Unable to parse bid");
+
+            let mapped_cards: String = map_cards_part2(&cards);
+            let hand_strength: char = get_hand_strength_part2(&cards);
+            let hand_sort_string = format!("{}_{}", hand_strength, mapped_cards);
+            hands.push(Hand {
+                bid,
+                initial_cards: cards,
+                mapped_cards,
+                hand_sort_string
+            });
+        }
+        hands.sort_by_key(|hand| hand.hand_sort_string.clone());
+        hands.reverse();
+        
+        let mut result:u64 = 0;
+        for (index, hand) in hands.iter().enumerate() {
+            result += (index +1) as u64 * hand.bid as u64;
+        }
+
+        result
     }
 }
 
-fn get_hand_strength(mapped_sorted_cards: &str) -> char {
+fn get_hand_strength_part2(cards: &str) -> char {
+    let non_joker_cards: Vec<char> = cards.chars().filter(|c| *c!='J').collect();
+    let joker_cards: Vec<char> = cards.chars().filter(|c| *c=='J').collect();
     // Five of a kind
-    if is_five_of_a_kind(mapped_sorted_cards) {
+    if is_five_of_a_kind_w_jokers(&non_joker_cards, joker_cards.len()) {
         return '0';
     }
     // Four of a kind
-    else if is_four_of_a_kind(mapped_sorted_cards) {
+    else if is_four_of_a_kind_w_jokers(&non_joker_cards, joker_cards.len()) {
         return '1';
     }
     // Full house
-    else if is_full_house(mapped_sorted_cards) {
+    else if is_full_house_w_jokers(&non_joker_cards, joker_cards.len()) {
         return '2';
     }
     // Three of a kind
 
-    else if is_three_of_a_kind(mapped_sorted_cards) {
+    else if is_three_of_a_kind_w_jokers(&non_joker_cards, joker_cards.len()) {
         return '3';
     }
     // Two pair
-    else if is_two_pair(mapped_sorted_cards) {
+    else if is_two_pair_w_jokers(&non_joker_cards, joker_cards.len()) {
         return '4';
     }
     // One pair
-    else if is_one_pair(mapped_sorted_cards) {
+    else if is_one_pair_w_jokers(&non_joker_cards, joker_cards.len()) {
+        return '5';
+    }
+    // High card
+    else {
+        return '6';
+    }
+}
+
+fn is_five_of_a_kind_w_jokers(non_joker_cards: &Vec<char>, number_of_jokers: usize) -> bool {
+    if number_of_jokers == 5 {
+        return true;
+    }
+    else {
+        let first = non_joker_cards.first().expect("Unable to get first card");
+        return non_joker_cards.iter().all(|c| c==first);
+    }
+}
+
+fn is_four_of_a_kind_w_jokers(non_joker_cards: &Vec<char>, number_of_jokers: usize) -> bool {
+    let non_joker_cards_string = non_joker_cards.iter().collect::<String>();
+    let card_counts = get_card_counts(non_joker_cards_string.as_str());
+
+    *card_counts.values().max().expect("Unable to get value max") + number_of_jokers as i32 == 4
+}
+fn is_full_house_w_jokers(non_joker_cards: &[char], number_of_jokers: usize) -> bool {
+    // at this point we can only have MAX 2 jokers, otherwise we would have matched four of a kind.
+
+    let non_joker_cards_string = non_joker_cards.iter().collect::<String>();
+    let card_counts = get_card_counts(non_joker_cards_string.as_str());
+    
+    let mut card_counts: Vec<i32> = card_counts.values().map(|v| *v).collect();
+    card_counts.sort_unstable();
+    card_counts.reverse();
+
+    if card_counts.len() > 1 && card_counts[0] + card_counts[1] + number_of_jokers as i32 == 5 {
+        return true;
+    }
+    else if card_counts.len() == 1 {
+        return true;
+    }
+    else {
+        return false;
+    }
+    
+}
+fn is_three_of_a_kind_w_jokers(non_joker_cards: &[char], number_of_jokers: usize) -> bool {
+    let non_joker_cards_string = non_joker_cards.iter().collect::<String>();
+    let card_counts = get_card_counts(non_joker_cards_string.as_str());
+    
+    let mut card_counts: Vec<i32> = card_counts.values().map(|v| *v).collect();
+    card_counts.sort_unstable();
+    card_counts.reverse();
+
+    if card_counts.len() >= 1 && card_counts[0] + number_of_jokers as i32 == 3 {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+fn is_two_pair_w_jokers(non_joker_cards: &[char], number_of_jokers: usize) -> bool {
+    let non_joker_cards_string = non_joker_cards.iter().collect::<String>();
+    let card_counts = get_card_counts(non_joker_cards_string.as_str());
+    
+    let mut card_counts: Vec<i32> = card_counts.values().map(|v| *v).collect();
+    card_counts.sort_unstable();
+    card_counts.reverse();
+
+    if card_counts.len() > 1 && card_counts[0] + card_counts[1] + number_of_jokers as i32 == 4 {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+fn is_one_pair_w_jokers(non_joker_cards: &[char], number_of_jokers: usize) -> bool {
+    let non_joker_cards_string = non_joker_cards.iter().collect::<String>();
+    let card_counts = get_card_counts(non_joker_cards_string.as_str());
+    
+    let mut card_counts: Vec<i32> = card_counts.values().map(|v| *v).collect();
+    card_counts.sort_unstable();
+    card_counts.reverse();
+
+    if card_counts.len() >= 1 && card_counts[0]  + number_of_jokers as i32 == 2 {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+fn map_cards_part2(cards: &str) -> String {
+    let mut mapped_cards = Vec::<char>::new();
+    for c in cards.chars() {
+        match c {
+            'A' => mapped_cards.push('A'),
+            'K' => mapped_cards.push('B'),
+            'Q' => mapped_cards.push('C'),
+            'T' => mapped_cards.push('E'),
+            '9' => mapped_cards.push('F'),
+            '8' => mapped_cards.push('G'),
+            '7' => mapped_cards.push('H'),
+            '6' => mapped_cards.push('I'),
+            '5' => mapped_cards.push('J'),
+            '4' => mapped_cards.push('K'),
+            '3' => mapped_cards.push('L'),
+            '2' => mapped_cards.push('M'),
+            '1' => mapped_cards.push('N'),
+            'J' => mapped_cards.push('O'),
+            _ => panic!("Unexpectec card")
+        }
+    }
+
+    mapped_cards.iter().collect()
+}
+
+fn get_hand_strength(cards: &str) -> char {
+    // Five of a kind
+    if is_five_of_a_kind(cards) {
+        return '0';
+    }
+    // Four of a kind
+    else if is_four_of_a_kind(cards) {
+        return '1';
+    }
+    // Full house
+    else if is_full_house(cards) {
+        return '2';
+    }
+    // Three of a kind
+
+    else if is_three_of_a_kind(cards) {
+        return '3';
+    }
+    // Two pair
+    else if is_two_pair(cards) {
+        return '4';
+    }
+    // One pair
+    else if is_one_pair(cards) {
         return '5';
     }
     // High card
@@ -142,10 +312,8 @@ fn is_full_house(mapped_sorted_cards: &str) -> bool {
 fn is_four_of_a_kind(mapped_sorted_cards: &str) -> bool {
     let card_counts = get_card_counts(mapped_sorted_cards);
 
-     *card_counts.values().max().expect("Unable to get value max") == 4 
+     *card_counts.values().max().expect("Unable to get value max") == 4
 }
-
-
 
 fn is_five_of_a_kind(mapped_sorted_cards: &str) -> bool {
     let first = mapped_sorted_cards.chars().nth(0).expect("no elements");
