6e017fa939dc8c48cd0097b3e823f92ef3462aec
torbjornmolin <37022228+torbjornmolin@users.noreply.github.com>
2023-12-10 22:02:28 +0100

    Prepared day11

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use day10::Day10;
+use day11::Day11;
 
-pub mod day10;
+pub mod day11;
 pub mod utils;
 
 fn main() {
-    let day = Day10;
+    let day = Day11;
 
     day.run_part_1();
     day.run_part_2();
